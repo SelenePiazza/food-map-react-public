b63d1ec106dcbd0f7b289cf4ddf1c2e4454b46b0
Selene Piazza <selene.piazza.tc@gmail.com>
2026-09-20 12:31:55 +0200

    release

--- assets/index-CsFSb0kH.js
+++ assets/index-CsFSb0kH.js
@@ -23728,7 +23728,14 @@ const Map$1 = ({ places = [], selectedPlace = null, onSelectPlace }) => {
         center: mapRef.current.getCenter(),
         zoom: mapRef.current.getZoom()
       };
-      mapRef.current.setView([selectedPlace.lat + 3e-3, selectedPlace.lng], 16);
+      const zoom = 16;
+      const markerPoint = mapRef.current.project(
+        [selectedPlace.lat, selectedPlace.lng],
+        zoom
+      );
+      markerPoint.y -= 120;
+      const shiftedCenter = mapRef.current.unproject(markerPoint, zoom);
+      mapRef.current.setView(shiftedCenter, zoom);
       showPopup(selectedPlace);
     } else {
       if (previousMapStateRef.current) {
